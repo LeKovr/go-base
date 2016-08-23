99f8d17d79321a62b31abfcb647c27e854fb9431
Alexey Kovrizhkin <jean@tender.pro>
2016-08-23 20:52:04 +0300

    added notls & mail skip if 'from' is empty

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -1,6 +1,7 @@
 package mailer
 
 import (
+	"crypto/tls"
 	"gopkg.in/gomail.v2"
 
 	"github.com/LeKovr/go-base/logger"
@@ -13,6 +14,7 @@ import (
 type Flags struct {
 	Port     int    `long:"smtp_port"  default:"25"        description:"SMTP server port"`
 	Host     string `long:"smtp_host"  default:"localhost" description:"SMTP server host"`
+	NoTLS    bool   `long:"smtp_notls" description:"Disable TLS cert checking"`
 	Login    string `long:"smtp_login" description:"SMTP sender login"`
 	Pass     string `long:"smtp_pass"  description:"SMTP sender password"`
 	Copy     string `long:"smtp_copy"  description:"SMTP bcopy address"`
@@ -43,6 +45,10 @@ func New(cfg *Flags, log *logger.Log, options ...func(a *App) error) (*App, erro
 // Send used to send emails
 func (a App) Send(email, name, subject, buf string, files []string) (err error) {
 
+	if a.Config.From == "" {
+		a.Log.Warn("Mail config field FROM does not set. Skip mailing")
+		return
+	}
 	msg := gomail.NewMessage()
 	if a.Config.FromName == "" {
 		msg.SetHeader("From", a.Config.From)
@@ -55,13 +61,16 @@ func (a App) Send(email, name, subject, buf string, files []string) (err error) 
 	}
 	msg.SetHeader("Subject", subject)
 
-	a.Log.Debugf("Mail header: %+v", msg)
 	msg.SetBody("text/html", buf)
 
 	for _, name := range files {
 		msg.Attach(name)
 	}
 	mailer := gomail.NewDialer(a.Config.Host, a.Config.Port, a.Config.Login, a.Config.Pass)
+
+	if a.Config.NoTLS {
+		mailer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	}
 	err = mailer.DialAndSend(msg)
 	return
 }
