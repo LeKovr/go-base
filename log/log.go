e2d8736bc8139668383133219a93c877e04d3fb2
Alexey Kovrizhkin <jean@tender.pro>
2017-10-29 00:32:47 +0300

    log.Logger interface

---
+++ log/log.go
@@ -0,0 +1,16 @@
+/*
+Package log defines log interface.
+
+Интерфейс logger.Entry используется для разделения библиотеки журналирования (например, logrus)
+и кода, который это журналирование использует (например, lib/grpcapi, lib/boltdb).
+
+Интерфейс содержит сигнатуры стандартных методов журналирования и WithField, который у базовой библиотеки (logrus) возвращает внутренний тип, а не этот интерфейс. Поэтому для WithField нужна обертка (см lib/logger).
+*/
+package log
+
+// Logger is an interface which allows mocks
+type Logger interface {
+	Printf(format string, v ...interface{})
+	Print(v ...interface{})
+	Println(v ...interface{})
+}
