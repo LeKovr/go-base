c9393dde724e619ae05ddf71cc32e346365443f3
Alexey Kovrizhkin <jean@tender.pro>
2017-12-23 18:02:01 +0300

    Logger iface completed

--- log/log.go
+++ log/log.go
@@ -10,7 +10,13 @@ package log
 
 // Logger is an interface which allows mocks
 type Logger interface {
-	Printf(format string, v ...interface{})
+	Fatal(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	Fatalln(v ...interface{})
+	Panic(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Panicln(v ...interface{})
 	Print(v ...interface{})
+	Printf(format string, v ...interface{})
 	Println(v ...interface{})
 }
